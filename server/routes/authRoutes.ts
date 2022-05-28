d1ebb52430bc20059b4435319addaf40fcf18754
Michal Radomski <michalra@op.pl>
2022-05-29 01:52:33 +0200

    refactor done2

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,4 +1,6 @@
-import passport from "passport";
+export {}; //* Cannot redeclare block-scoped variable 'passport'.ts(2451)
+// import passport from "passport";
+const passport = require("passport");
 
 module.exports = (app: {get: (arg0: string, arg1: any) => void}) => {
   app.get(
