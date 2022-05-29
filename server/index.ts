426f465179d8a8646abdae0d9a9e57e527f1161e
Michal Radomski <michalra@op.pl>
2022-05-29 22:25:30 +0200

    MongoDB was added

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mongoDB_key = require("./config/keys").mongoURI;
+
+// console.log({mongoDB_key});
 
 require("./services/passport"); //* Calling the function immediately instead of storing it in a variable
 // const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoDB_key, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to the MongoDB"))
+  .catch((error: string) => console.log({error}));
 
 // import {Request, Response} from "express";
 // app.get("/", (req: Request, res: Response) => {
