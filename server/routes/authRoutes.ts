fd901c5bd4eda82e160463e1629929a2c3e6065d
Michal Radomski <michalra@op.pl>
2022-05-30 16:02:37 +0200

    logOut was added

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 export {}; //* Cannot redeclare block-scoped variable 'passport'.ts(2451)
 // import passport from "passport";
 const passport = require("passport");
+import {Request, Response} from "express";
 
 module.exports = (app: {get: (arg0: string, arg1: any) => void}) => {
   app.get(
@@ -11,4 +12,13 @@ module.exports = (app: {get: (arg0: string, arg1: any) => void}) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google", {failureRedirect: "/login"}));
+
+  app.get("/api/logout", (req: Request, res: Response) => {
+    res.send(req.user);
+    req.logout();
+  });
+
+  app.get("/api/current_user", (req: Request, res: Response) => {
+    res.send(req.user);
+  });
 };
