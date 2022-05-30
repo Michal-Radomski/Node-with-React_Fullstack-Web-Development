987f03bdf6816c4cc643759112ab0d6af3b51256
Michal Radomski <michalra@op.pl>
2022-05-30 16:26:03 +0200

    temp commit1

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -11,14 +11,24 @@ module.exports = (app: {get: (arg0: string, arg1: any) => void}) => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google", {failureRedirect: "/login"}));
+  app.get("/auth/google/callback", passport.authenticate("google"));
 
   app.get("/api/logout", (req: Request, res: Response) => {
-    res.send(req.user);
+    // res.send(req.user);
     req.logout();
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req: Request, res: Response) => {
+    console.log("req.user:", req.user);
     res.send(req.user);
+  });
+
+  app.get("/login", (_req: Request, res: Response) => {
+    res.send("Log In");
+  });
+
+  app.get("/", (_req: Request, res: Response) => {
+    res.send("Home Page");
   });
 };
