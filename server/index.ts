b985960eb963074739f36de83aa9d0808b85ae2c
Michal Radomski <michalra@op.pl>
2022-05-30 15:47:07 +0200

    cookieSession was added

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
 const mongoDB_key = require("./config/keys").mongoURI;
+const cookie_Key = require("./config/keys").cookieKey;
 
 // console.log({mongoDB_key});
 
 require("./models/User");
 require("./services/passport"); //* Calling the function immediately instead of storing it in a variable
 // const authRoutes = require("./routes/authRoutes");
-
-const app = express();
 
 // Connect to MongoDB
 mongoose
@@ -19,11 +21,24 @@ mongoose
   .then(() => console.log("Connected to the MongoDB"))
   .catch((error: string) => console.log({error}));
 
+const app = express();
+
 // import {Request, Response} from "express";
 // app.get("/", (req: Request, res: Response) => {
 //   console.log("req.ip", req.ip);
 //   res.send({hi: "there"});
 // });
+
+app.use(
+  cookieSession({
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    keys: [cookie_Key],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // authRoutes(app);
 //* below the same
@@ -35,6 +50,6 @@ app.listen(PORT, () => {
   console.log(`The server is running on port: ${PORT}`);
 });
 
-//------------
+//* For testing only
 const now: Date = new Date();
 console.log("Current time:", now.toLocaleString());
