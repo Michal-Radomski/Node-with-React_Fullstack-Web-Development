b4e291657e692571ca4542af7debf76fd7f3bb70
Michal Radomski <michalra@op.pl>
2022-06-01 23:43:56 +0200

    React-Router-Dom was added2

--- server/client/src/components/App.tsx
+++ server/client/src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import "./App.scss";
 import Header from "./Header";
 
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+const Dashboard = (): JSX.Element => <h2>Dashboard</h2>;
+const SurveyNew = (): JSX.Element => <h2>SurveyNew</h2>;
+const Landing = (): JSX.Element => <h2>Landing</h2>;
+const NotFound = (): JSX.Element => <h2>Page Not Found</h2>;
 
 function App(): JSX.Element {
   return (
@@ -16,9 +17,14 @@ function App(): JSX.Element {
           <div>
             {/* Header always visible */}
             <Header />
-            <Route path="/" exact={true} component={Landing} />
-            <Route path="/surveys" exact={true} component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+            <Switch>
+              <Route path="/" exact={true} component={Landing} />
+              <Route path="/surveys" exact={true} component={Dashboard} />
+              <Route path="/surveys/new" component={SurveyNew} />
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
           </div>
         </Router>
       </div>
