f71ba8ca743b422989f076de1c92e7c4b813fbfe
Michal Radomski <michalra@op.pl>
2022-06-02 00:11:34 +0200

    materializa-css was added

--- server/client/src/components/App.tsx
+++ server/client/src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import "materialize-css/dist/css/materialize.min.css";
 import "./App.scss";
 import Header from "./Header";
 
