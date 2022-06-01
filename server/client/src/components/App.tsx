2ae02542ff273d6478f9af0fe8719e362fe043e1
Michal Radomski <michalra@op.pl>
2022-06-01 19:55:57 +0200

    React setup - initial commit2

---
+++ server/client/src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.scss";
+
+function App(): JSX.Element {
+  return <React.Fragment>Hi there!</React.Fragment>;
+}
+
+export default App;
--- server/client/src/index.tsx
+++ server/client/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+
+const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root") as HTMLElement
+  root
 );
