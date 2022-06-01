4e7525d860c1598ec1e939aeccc51183c9d31288
Michal Radomski <michalra@op.pl>
2022-06-01 18:31:14 +0200

    React setup - initial commit

---
+++ server/client/src.BACK/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <React.Fragment>
+      App
+      <br />
+      <a href="/auth/google">Sign In With Google</a>
+    </React.Fragment>
+  );
+}
+
+export default App;
