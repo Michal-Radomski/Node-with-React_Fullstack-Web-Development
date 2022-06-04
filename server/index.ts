b2188a7d9180e892d33617982cda3e4fdad6b203
Michal Radomski <michalra@op.pl>
2022-06-04 21:50:58 +0200

    stripe was added to the backend

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 const mongoDB_key = require("./config/keys").mongoURI;
 const cookie_Key = require("./config/keys").cookieKey;
@@ -24,6 +25,7 @@ mongoose
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     // Cookie Options
--- server/routes/billingRoutes.ts
+++ server/routes/billingRoutes.ts
@@ -2,6 +2,16 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 import {Request, Response} from "express";
 
-module.exports = (app: {post: (arg0: string, arg1: any) => void}) => {
-  app.post("/api/stripe", (req: Request, res: Response) => {});
+module.exports = (app: {post: (arg0: string, arg1: Object) => void}) => {
+  app.post("/api/stripe", async (req: Request, _res: Response) => {
+    // console.log("req.body:", req.body);
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id,
+    });
+    // console.log({charge});
+    // console.log({_res});
+  });
 };
