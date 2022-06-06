0ee30306974494b16d8dca35713b643b595358d8
Michal Radomski <michalra@op.pl>
2022-06-06 23:00:24 +0200

    routing in productin done

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 const express = require("express");
+import {Request, Response} from "express";
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
@@ -43,6 +44,18 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 const PORT = (process.env.PORT || 5000) as number;
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req: Request, res: Response) => {
+    console.log("req.ip:", req.ip);
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`The server is running on port: ${PORT}`);
