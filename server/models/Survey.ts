00839c9353b1a7c7c4cae75e05937c6440e9d715
Michal Radomski <michalra@op.pl>
2022-06-07 23:09:47 +0200

    Survey.ts was updated

--- server/models/Survey.ts
+++ server/models/Survey.ts
@@ -10,6 +10,9 @@ const surveySchema = new Schema({
   recipients: [RecipientSchema], //* Array of RecipientSchema records
   yes: {type: Number, default: 0},
   no: {type: Number, default: 0},
+  _user: {type: Schema.Types.ObjectId, ref: "User"}, //* Id of user who owns the record; _underscore means it's  relationship field
+  dateSent: Date,
+  lastResponded: Date,
 });
 
 mongoose.model("surveys", surveySchema);
