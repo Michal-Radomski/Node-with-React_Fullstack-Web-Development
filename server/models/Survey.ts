c732fbf6fa0cdffeaf82759c53f87f6a6aff3ecb
Michal Radomski <michalra@op.pl>
2022-06-07 23:04:18 +0200

    Recipients.ts was added

--- server/models/Survey.ts
+++ server/models/Survey.ts
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const {Schema} = mongoose;
+const RecipientSchema = require("./Recipients");
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  // recipients: [String],
+  recipients: [RecipientSchema], //* Array of RecipientSchema records
+  yes: {type: Number, default: 0},
+  no: {type: Number, default: 0},
 });
 
 mongoose.model("surveys", surveySchema);
