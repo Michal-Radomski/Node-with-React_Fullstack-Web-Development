21bf529f478213897580f5f7bb707254f4b1df29
Michal Radomski <michalra@op.pl>
2022-06-10 00:54:50 +0200

    Dashbord updated

--- server/client/src/components/Dashboard.tsx
+++ server/client/src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 const Dashboard = (): JSX.Element => {
   return (
     <React.Fragment>
-      <h3>Dashboard</h3>
+      <h3 style={{textAlign: "center"}}>Dashboard</h3>
       <div style={{backgroundColor: "lightyellow", textAlign: "center"}}>
-        <h5>To test type:</h5>
+        <h5>Type to Test:</h5>
         <p>Card Number: 4242424242424242</p>
         <p>CVC: Any 3 digits</p>
         <p>Date: Any future date</p>
@@ -14,6 +16,12 @@ const Dashboard = (): JSX.Element => {
             Stripe Docs
           </a>
         </p>
+      </div>
+
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="large material-icons">add</i>
+        </Link>
       </div>
     </React.Fragment>
   );
